cf56c5f60624afc1eb9d7fae0ae1bfe18fe39ad5
Ahmad Aulia Noorhakim <aaulia@gmail.com>
2021-02-24 04:25:19 +0700

    Replace storyboards with xib

--- MovieApp/Core/AppDelegate.swift
+++ MovieApp/Core/AppDelegate.swift
@@ -13,7 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let homeViewController = HomeViewController()
+        let appUINavController = UINavigationController(rootViewController: homeViewController)
+        
+        window?.rootViewController = appUINavController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
