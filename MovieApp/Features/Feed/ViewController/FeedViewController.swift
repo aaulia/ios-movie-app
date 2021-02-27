66567beb5f857d100069e7c1139bf0ca3886819d
Ahmad Aulia Noorhakim <aaulia@gmail.com>
2021-02-27 21:25:04 +0700

    Refactor FeedViewController to use UICollectionViewController

--- MovieApp/Features/Feed/ViewController/FeedViewController.swift
+++ MovieApp/Features/Feed/ViewController/FeedViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 import Nuke
 
-final class FeedViewController: UIViewController {
+final class FeedViewController: UICollectionViewController {
     
-    @IBOutlet weak var movieCollection: UICollectionView!
+    private let cellCorderRadius: CGFloat = 8
+    private let movieCellPerLine: CGFloat = 3
+    private let movieCellSpacing: CGFloat = 10
+    private let movieCellRatio  : CGFloat = 0.66666666666 // = 2 / 3
     
-    private let refreshControl = UIRefreshControl()
-    private let brokenImage = UIImage(named: "icon_broken_image")?.withTintColor(UIColor.systemGray4)
+    private let contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    private let brokenImage  = UIImage(named: "icon_broken_image")?.withTintColor(UIColor.systemGray4)
     
     
     private let feedType: FeedType
@@ -29,7 +32,7 @@ final class FeedViewController: UIViewController {
     init(withType type: FeedType) {
         self.feedType = type
 
-        super.init(nibName: nil, bundle: nil)
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
         setup()
     }
     
@@ -44,12 +47,17 @@ final class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        refreshControl.addTarget(self, action: #selector(onRefresh), for: .valueChanged)
+        collectionView.refreshControl  = UIRefreshControl()
+        collectionView.backgroundColor = UIColor.systemBackground
+        collectionView.contentInset    = self.contentInset
+        collectionView.delegate        = self
+        collectionView.dataSource      = self
+
+        collectionView.refreshControl?.addTarget(self, action: #selector(onRefresh), for: .valueChanged)
         
-        movieCollection.refreshControl = refreshControl
-        movieCollection.delegate       = self
-        movieCollection.dataSource     = self
-        movieCollection.register(UINib(nibName: "MovieViewCell", bundle: nil), forCellWithReuseIdentifier: "MovieCell")
+        let nibMovieCell = UINib(nibName: "MovieViewCell", bundle: nil)
+        collectionView.register(nibMovieCell, forCellWithReuseIdentifier: "MovieCell")
+        
         
         output.fetchMovies(Feed.Request(feedType: self.feedType))
     }
@@ -70,65 +78,22 @@ final class FeedViewController: UIViewController {
         tabBarItem.image = UIImage(named: feedType.icon)
     }
     
-}
-
-extension FeedViewController: FeedPresenterOutput {
-    
-    func showLoading() {
-        refreshControl.beginRefreshing()
-    }
-    
-    func hideLoading() {
-        refreshControl.endRefreshing()
-    }
-    
-    func showFailure(_ error: Error) {
-        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
-        self.present(alert, animated: true)
-    }
-    
-    func showSuccess(_ model: Feed.ViewModel) {
-        viewModel = model
-        movieCollection.reloadData()
-    }
-    
-    
-}
-
-extension FeedViewController: UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let movie = viewModel.movies[indexPath.row]
         router.routeToDetails(movieId: movie.id)
     }
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        let spacing: CGFloat = 10 * 4
-        let content: CGFloat = movieCollection.bounds.width - spacing
-        
-        let w = content / 3
-        let h = (w / 2) * 3
-        
-        return CGSize(width: w, height: h)
-    }
-    
-}
-
-extension FeedViewController: UICollectionViewDataSource {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.movies.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = movieCollection.dequeueReusableCell(withReuseIdentifier: "MovieCell", for: indexPath)
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCell", for: indexPath)
         let data = viewModel.movies[indexPath.row]
         
         if let movieCell = cell as? MovieViewCell {
-            movieCell.layer.cornerRadius = 8.0
-            movieCell.layer.cornerCurve = .continuous
+            movieCell.layer.cornerRadius = cellCorderRadius
+            movieCell.layer.cornerCurve  = .continuous
             
             if let poster = data.image {
                 movieCell.imagePoster.contentMode = .scaleAspectFill
@@ -140,6 +105,44 @@ extension FeedViewController: UICollectionViewDataSource {
         }
         
         return cell
+    }
+    
+}
+
+extension FeedViewController: FeedPresenterOutput {
+    
+    func showLoading() {
+        collectionView.refreshControl?.beginRefreshing()
+    }
+    
+    func hideLoading() {
+        collectionView.refreshControl?.endRefreshing()
+    }
+    
+    func showFailure(_ error: Error) {
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
+    func showSuccess(_ model: Feed.ViewModel) {
+        viewModel = model
+        collectionView.reloadData()
+    }
+    
+}
+
+extension FeedViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let spacing: CGFloat = movieCellSpacing * (movieCellPerLine + 1)
+        let content: CGFloat = collectionView.bounds.width - spacing
+        
+        let w = content / movieCellPerLine
+        let h = w / movieCellRatio
+        
+        return CGSize(width: w, height: h)
     }
     
 }
